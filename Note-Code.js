18aeac26adcf6b7f83a7fa7fdd77ee55eb15989b
Md Arfan Khan <khan188993@gmail.com>
2022-01-29 23:12:10 +0600

    3)practice challange add

--- Note-Code.js
+++ Note-Code.js
@@ -330,6 +330,35 @@ function feetToMile(feetNumber) {
   
   console.log(tinyFriend(["raju", "Shuvo", "kajol"]));
 
+
+  // প্রাকটিস চ্যালেঞ্জ-৩ টা:
+
+// ১. leapYear() নামে ফাংশন লিখো এবং নেক্সট ইয়ার অর্থাৎ ২০২৩ কি leap year নাকি সেটা চেক করো। Leap year হলে ফাংশন true রিটার্ন করবে আর leap year না হলে false রিটার্ন করবে।
+
+function leapYear(year) {
+  return year % 4 === 0 && (!(year % 100 === 0) || year % 400 === 0)
+    ? `${year} is a Leap Year`
+    : `${year} is not a Leap Year`;
+}
+
+console.log(leapYear(2100));
+
+// ২. তোমার বয়স কি odd নাকি even সংখ্যা সেটা চেক কর একটা ফাংশন দিয়ে। সেই ফাংশনকে কোন সংখ্যা প্যারামিটার হিসেবে দিলে, সেই সংখ্যা Even হলে ফাংশন true রিটার্ন করবে আর Odd হলে false রিটার্ন করবে।
+
+
+function ageEvenOrOdd(age) {
+return age % 2 === 0 ? true : false;
+}
+console.log(ageEvenOrOdd(24));
+
+// ৩. এমন একটা ফ্যাংশনা লিখো যেটাকে তুমি ঘন্টাকে ইনপুট প্যারামিটার হিসেবে দিবে। আর সে সেই ঘন্টাকে মিনিটে কনভার্ট করে মিনিট রিটার্ন করবে।
+function hourToMinute(hour) {
+return hour * 60; //for minutes
+return hour * 60 * 60; //for second
+}
+console.log(hourToMinute(24));
+
+
 //*)Programming hero solving End 
 
 
